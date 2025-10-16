d6870b03d6dc158653b9e78e756917cca974d28b
Muhammad Raehan Putra H <raehan.ph@gmail.com>
2025-10-16 13:21:18 +0700

    cek

--- app/api/hanielsen/route.js
+++ app/api/hanielsen/route.js
@@ -1,23 +1,19 @@
-import pool from '../../../lib/db';
-
+import pool from "../../../lib/db";
 
 export async function GET() {
   try {
-    const [rows] = await pool.query('SELECT * FROM users');
+    const [rows] = await pool.query("SELECT * FROM users");
 
     return new Response(JSON.stringify(rows), {
       status: 200,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
-
   } catch (err) {
-    console.error('Database error:', err);
+    console.error("Database error:", err);
 
-    return new Response(JSON.stringify({ error: 'Database error' }), {
+    return new Response(JSON.stringify({ error: "Database error" }), {
       status: 500,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
   }
 }
-
-
