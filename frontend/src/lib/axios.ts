13172012b7e4804f2d92a14e5a06295e8940784a
Gvanceren <aaronkho0211@email.com>
2025-12-24 14:29:01 +0700

    BARU NI

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,38 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-// (opsional tapi sangat disarankan)
-// otomatis kirim token kalau ada
-api.interceptors.request.use(
-  (config) => {
-    if (typeof window !== "undefined") {
-      const token = localStorage.getItem("token");
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// (opsional) logging error biar gampang debug
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.error(
-      "API ERROR:",
-      error.response?.status,
-      error.response?.data
-    );
-    return Promise.reject(error);
-  }
-);
 
 export default api;
